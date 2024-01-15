d3cc0aa21621c9c7404bdb2fb9d7abca57b24c0d
zP0 <zP@solarsystem.coffee>
2024-01-14 17:12:40 -0900

    hackz

---
+++ hackz/hackz.js
@@ -0,0 +1,16 @@
+document.querySelector("h1").onmouseover = event => {
+  const letters = "ABCDEFGHLMNOPQRSTUVWXYZ0123456789";
+  let iteration = 0;
+  const interval = setInterval(() => {
+  event.target.innerText = event.target.innerText.split("")
+  .map((letter, index) => {
+    if(index < iteration){
+      return event.target.dataset.value[index];
+    }
+    return letters[Math.floor(Math.random() * 31)]
+  })
+    .join("");
+    if(iteration >= event.target.dataset.value.length) clearInterval(interval);
+    iteration += 1/4;
+  }, 30);
+}
